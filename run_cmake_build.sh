8568d504dc642ae33a9aa89c3499d2ac6bcf0d08
wanghongyu <1354998814@qq.com>
2023-01-04 10:13:59 +0800

    add the script and CMake for superlu_dist install (have gpu)

--- run_cmake_build.sh
+++ run_cmake_build.sh
@@ -1,50 +1,23 @@
 #!/bin/bash
-
-## if [ !$?NERSC_HOST ]
-if [ -z $NERSC_HOST ]
-then
-    echo "NERSC_HOST undefined"
-    
-elif [ "$NERSC_HOST" == "cori" ]
-then
-#    rm -fr 64-build; mkdir 64-build; cd 64-build;
-#    export PARMETIS_ROOT=~/Cori/lib/parmetis-4.0.3-64
-    rm -fr cori-build; mkdir cori-build; cd cori-build;
-    export PARMETIS_ROOT=~/Cori/lib/parmetis-4.0.3
-#    export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/shared-build
-    export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/build/Linux-x86_64
-    export COMBBLAS_ROOT=/project/projectdirs/sparse/xiaoye/Cori/combinatorial-blas-2.0/CombBLAS
-#    export COMBBLAS_ROOT=/project/projectdirs/sparse/xiaoye/Cori/CombBLAS_beta_16_2
-    cmake .. \
-    -DCMAKE_Fortran_COMPILER=ftn \
-    -DCMAKE_C_FLAGS="-std=c99 -fPIC -DPRNTlevel=0" \
-    -DTPL_PARMETIS_INCLUDE_DIRS="${PARMETIS_ROOT}/include;${PARMETIS_ROOT}/metis/include" \
-    -DTPL_PARMETIS_LIBRARIES="${PARMETIS_BUILD_DIR}/libparmetis/libparmetis.a;${PARMETIS_BUILD_DIR}/libmetis/libmetis.a" \
-    -DTPL_COMBBLAS_INCLUDE_DIRS="${COMBBLAS_ROOT}/_install/include;${COMBBLAS_ROOT}/Applications/BipartiteMatchings" \
-    -DTPL_COMBBLAS_LIBRARIES="${COMBBLAS_ROOT}/_install/lib/libCombBLAS.a" \
-    -DTPL_ENABLE_INTERNAL_BLASLIB=OFF \
-    -DTPL_ENABLE_COMBBLASLIB=ON \
-    -DMPIEXEC_EXECUTABLE=/usr/bin/srun \
-    -DCMAKE_INSTALL_PREFIX=.
-#   -DTPL_ENABLE_CUDALIB=TRUE \
-#    -DTPL_BLAS_LIBRARIES="-mkl" \
-#    -DXSDK_INDEX_SIZE=64
-#    -DXSDK_ENABLE_Fortran=TRUE \
-#    -DTPL_ENABLE_INTERNAL_BLASLIB=OFF \
-#    -DTPL_ENABLE_COMBBLASLIB=FALSE \
-#    -DTPL_ENABLE_LAPACKLIB=FALSE \
-#    -DCMAKE_EXE_LINKER_FLAGS="-shared" \
-#    -DCMAKE_CXX_FLAGS="-std=c++14" \
-#    -DTPL_COMBBLAS_INCLUDE_DIRS="${COMBBLAS_ROOT}/_install/include;${COMBBLAS_ROOT}/_install/include/CombBLAS;${COMBBLAS_ROOT}/BipartiteMatchings"
-	
-fi
-
+spack load mpich@4.0.2%gcc@7.5.0
+spack load gcc@7.5.0
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH//\/usr\/local\/cuda\/compat:/}
+CUDA_HOME=/usr/local/cuda
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export CPATH=$CUDA_HOME/include:$CPATH
+export PATH=$CUDA_HOME/bin:$PATH
+NVARCH=`uname -s`_`uname -m`; export NVARCH
+NVCOMPILERS=/home/wanghongyu/nvhpc/Linux_x86_64; export NVCOMPILERS
+MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/22.9/compilers/man; export MANPATH
+PATH=$NVCOMPILERS/$NVARCH/22.9/compilers/bin:$PATH; export PATH
+spack load mpich@4.0.2%gcc@7.5.0
 THISHOST=`hostname -s`
 echo "host: $THISHOST"
-if [ "$THISHOST" == "ssg1" ]
+if [ "$THISHOST" == "znkxjs981" ]
 then
   rm -fr ssg1-build; mkdir ssg1-build; cd ssg1-build;
-  export PARMETIS_ROOT=~/lib/static/parmetis-4.0.3 
+  export PARMETIS_ROOT=/home/wanghongyu/learn/pexsi/parmetis-4.0.3 
 #  rm -fr int64-build; mkdir int64-build; cd int64-build;
 #  export PARMETIS_ROOT=~/lib/static/64-bit/parmetis-4.0.3 
   export PARMETIS_BUILD_DIR=${PARMETIS_ROOT}/build/Linux-x86_64
@@ -57,15 +30,21 @@ then
     -DCMAKE_C_FLAGS="-std=c99 -O3 -g -DPRNTlevel=0 -DDEBUGlevel=0" \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_CXX_COMPILER=mpicxx \
-    -DCMAKE_CXX_FLAGS="-std=c++11" \
-    -DCMAKE_Fortran_COMPILER=mpif90 \
+    -DCMAKE_CXX_FLAGS="-std=c++11 -L/usr/local/cuda/lib64 -lcudart -lcudadevrt" \
+    -DCMAKE_Fortran_COMPILER=mpifort \
+    -DCMAKE_Fortran_FLAGS="-L/usr/local/cuda/lib64 -lcudart -lcudadevrt"
     -DCMAKE_LINKER=mpicxx \
     -Denable_openmp=ON \
     -DTPL_ENABLE_INTERNAL_BLASLIB=OFF \
     -DTPL_ENABLE_COMBBLASLIB=OFF \
     -DTPL_ENABLE_LAPACKLIB=OFF \
     -DBUILD_SHARED_LIBS=OFF \
-    -DCMAKE_INSTALL_PREFIX=.
+    -DCMAKE_CUDA_ARCHITECTURES=80 \
+    -DCMAKE_CUDA_FLAGS="-I/usr/local/spack/opt/spack/linux-centos7-skylake_avx512/gcc-7.5.0/mpich-4.0.2-fokcm5f6j265rcne27n7bgbiv6ni3ryv/include" \
+    -DTPL_ENABLE_CUDALIB=ON \
+    -DCUDA_LIBRARIES="/usr/local/cuda/lib64/libcublas.so;/usr/local/cuda/lib64/libcudart.so;/usr/local/cuda/lib64/libcusparse.so" \
+    -DCMAKE_INSTALL_PREFIX="/home/wanghongyu/learn/pexsi/superlu_dist"  
+    make -j 48 VERBOSE=1 && make install
 fi
 #    -DXSDK_INDEX_SIZE=64 \
 #    -DXSDK_ENABLE_Fortran=TRUE \
